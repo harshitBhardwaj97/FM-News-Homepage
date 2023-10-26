37025ca5438a4ba6a91f31e1bdd7e94052ae20f6
Harshit Bhardwaj <harshitb.external@credable.in>
2023-10-26 20:14:18 +0530

    News Homepage Code Added

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primarySoftOrange: "hsl(35, 77%, 62%)",
+        primarySoftRed: "hsl(5, 85%, 63%)",
+        neutralOffWhite: "hsl(36, 100%, 99%)",
+        neutralGrayishBlue: "hsl(233, 8%, 79%)",
+        neutralDarkGrayishBlue: "hsl(236, 13%, 42%)",
+        neutralVeryDarkBlue: "hsl(240, 100%, 5%)",
+      },
+    },
+  },
+  plugins: [],
+};
